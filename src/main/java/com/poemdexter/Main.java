e4b7146ce8f5979d1e7ba5789fca191154a2e890
poemdexter <poemdexter@gmail.com>
2013-10-28 01:12:37 -0500

    fixed to maven recommended folder structure

--- src/main/java/com/poemdexter/Main.java
+++ src/main/java/com/poemdexter/Main.java
@@ -1,4 +1,4 @@
-package main.java.com.poemdexter;
+package com.poemdexter;
 
 import java.net.UnknownHostException;
 
--- src/main/java/com/poemdexter/Player.java
+++ src/main/java/com/poemdexter/Player.java
@@ -1,4 +1,4 @@
-package main.java.com.poemdexter;
+package com.poemdexter;
 
 import org.springframework.data.annotation.Id;
 
