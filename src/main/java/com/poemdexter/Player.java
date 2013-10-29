7ba03d56c06321b0aca448f2fbde0fa6b587a065
poemdexter <poemdexter@gmail.com>
2013-10-28 20:30:48 -0500

    converted project to intellij

--- src/main/java/com/poemdexter/Player.java
+++ src/main/java/com/poemdexter/Player.java
@@ -4,37 +4,46 @@ import org.springframework.data.annotation.Id;
 
 public class Player {
 
-    @Id
-    private String id;
+	@Id
+	private String id;
 
-    private String username;
-    private String password;
+	private String username;
+	private String password;
 
-    public Player() {}
+	public Player() {
+	}
 
-    public Player(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-    
-    public String getUsername() {
-        return username;
-    }
+	public Player(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    
-    public String getPassword() {
-        return password;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    @Override
-    public String toString() {
-        return String.format("Player [id=%s, username='%s', password='%s']", id, username, password);
-    }
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Player [id=%s, username='%s', password='%s']", getId(), username, password);
+	}
 }
--- src/main/java/com/poemdexter/PlayerRepository.java
+++ src/main/java/com/poemdexter/PlayerRepository.java
@@ -1,9 +1,10 @@
 package com.poemdexter;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface PlayerRepository extends MongoRepository<Player, String> {
-    
-    // automatically implemented by Spring Data Mongo
-    public Player findByUsername(String username);
+
+	// automatically implemented by Spring Data Mongo
+	public Player findByUsername(String username);
 }
